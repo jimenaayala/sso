e0dac481e35c2a9459697d1569f32597ef3f415e
EdgardoCozzo <edgardocozzo@gmail.com>
2024-08-23 17:57:23 -0300

    borre unos comentarios

--- SistemaSSO/src/main/java/com/sso/app/controller/InspeccionController.java
+++ SistemaSSO/src/main/java/com/sso/app/controller/InspeccionController.java
@@ -12,7 +12,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("api/inspeccion")
-@AllArgsConstructor // en el controller es necesario?
+@AllArgsConstructor
 @CrossOrigin
 public class InspeccionController {
 
