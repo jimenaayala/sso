0b35ba4210676be6d208149738f1719b458b65a8
EdgardoCozzo <edgardocozzo@gmail.com>
2025-04-03 13:15:31 -0300

    agregamos en Orden las Tablas de Inspeccion de MiniG,cougar,dv1 y de ensayo vh60, minig, dv1 y cougar..

--- SistemaSSO/src/main/java/com/sso/app/entity/Orden.java
+++ SistemaSSO/src/main/java/com/sso/app/entity/Orden.java
@@ -1,6 +1,13 @@
 package com.sso.app.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.sso.app.entity.ensayo.EnsayoCougar;
+import com.sso.app.entity.ensayo.EnsayoDv1;
+import com.sso.app.entity.ensayo.EnsayoMiniG;
+import com.sso.app.entity.ensayo.EnsayoVh60;
+import com.sso.app.entity.inspeccion.pcpcougarcd50.InspeccionPcpCougar;
+import com.sso.app.entity.inspeccion.pcpdv1.InspeccionPcpDV1;
+import com.sso.app.entity.inspeccion.pcpminig.InspeccionPcpMiniG;
 import com.sso.app.entity.inspeccion.pcpvh60.InspeccionPcpVh60;
 import jakarta.persistence.*;
 import lombok.*;
@@ -42,21 +49,50 @@ public class Orden {
     @JoinColumn(name = "equipo_id")
     private Equipo equipo;
 
-    @ManyToOne
-    @JoinColumn(name = "inspeccion_id")
-    private Inspeccion inspeccion;
+    //@ManyToOne
+    //@JoinColumn(name = "inspeccion_id")
+    //private Inspeccion inspeccion;
 
     @ManyToOne
     @JoinColumn(name = "inspeccionPcpVh60_id")
     private InspeccionPcpVh60 inspeccionPcpVh60;
 
     @ManyToOne
+    @JoinColumn(name = "inspeccionPcpMiniG_id")
+    private InspeccionPcpMiniG inspeccionPcpMiniG;
+
+    @ManyToOne
+    @JoinColumn(name = "inspeccionPcpDV1_id")
+    private InspeccionPcpDV1 inspeccionPcpDV1;
+
+    @ManyToOne
+    @JoinColumn(name = "inspeccionPcpCougar_id")
+    private InspeccionPcpCougar inspeccionPcpCougar;
+
+    @ManyToOne
     @JoinColumn(name = "recepcion_id")
     private Recepcion recepcion;
 
+    //@ManyToOne
+    //@JoinColumn(name = "ensayo_id")
+    //private Ensayo ensayo;
+
     @ManyToOne
-    @JoinColumn(name = "ensayo_id")
-    private Ensayo ensayo;
+    @JoinColumn(name = "ensayoVH60_id")
+    private EnsayoVh60 ensayoVh60;
+
+    @ManyToOne
+    @JoinColumn(name = "ensayoMiniG_id")
+    private EnsayoMiniG ensayoMiniG;
+
+    @ManyToOne
+    @JoinColumn(name = "ensayoDv1_id")
+    private EnsayoDv1 ensayoDv1;
+
+    @ManyToOne
+    @JoinColumn(name = "ensayoCougar_id")
+    private EnsayoCougar ensayoCougar;
+
     @ManyToOne
     @JoinColumn(name = "salida_id")
     private Salida salidaEtapa;
